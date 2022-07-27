cc0bbc570605c869cd9e20a58c1e773e8416e357
Jorge <jorgeharanda97@yahoo.com>
2022-07-27 16:43:53 -0700

    Adding a storage stack

--- services/functions/lambda.js
+++
@@ -1,7 +0,0 @@
-export const handler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Hello, Jorge! Your request was received at ${event.requestContext.time}.`,
-  };
-};
